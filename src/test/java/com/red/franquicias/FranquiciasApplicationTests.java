b06bdef98f7915f8595c62f87cd3eb1bb2a062c0
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-02-05 10:42:27 -0500

    chore: init redFranquicia spring initializr

---
+++ src/test/java/com/red/franquicias/FranquiciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red.franquicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
